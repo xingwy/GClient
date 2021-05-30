b26d265eaacc5ffaaa0e5b4c1370850a873fcfbc
xingwy <yuxingwei.code@foxmail.com>
2021-05-30 21:34:56 +0800

    constant文件

---
+++ define/constant.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#endif // CONSTANT_H
+
+#define BORDER 1
+#define GAME_BG_WIDTH 960
+#define GAME_BG_HEIGHT 540
+#define LOGIN_BG_WIDTH 958
+#define LOGIN_BG_HEIGHT 538
+
+#define GAME_BG_IMG ":/images/bg/bg_black.jpg"
+#define RES_LOGIN_BG_IMG ":/images/scene/s_login_v1.jpg"
